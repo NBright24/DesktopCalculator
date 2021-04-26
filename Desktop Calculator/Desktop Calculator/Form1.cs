e183f11c8ce78ae36fb042ef5274dcf21003a02b
NBright24 <brebz0324@gmail.com>
2021-04-26 19:01:44 +0800

    Added Buttons
    Buttons are added but they are useless, for now.

--- Desktop Calculator/Desktop Calculator/Form1.cs
+++ Desktop Calculator/Desktop Calculator/Form1.cs
@@ -17,5 +17,19 @@ namespace Desktop_Calculator
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
