16615ab1176e4238d8e763c1d666cc24580ed048
NBright24 <78198636+NBright24@users.noreply.github.com>
2021-07-08 13:48:42 +0800

    Modified Controls
    For some reasons, keyboard input was disabled.

--- Desktop Calculator/Desktop Calculator/CalculatorFace.Designer.cs
+++ Desktop Calculator/Desktop Calculator/CalculatorFace.Designer.cs
@@ -434,6 +434,7 @@ namespace Desktop_Calculator
             this.ValueBox.Location = new System.Drawing.Point(3, 65);
             this.ValueBox.MaxLength = 20;
             this.ValueBox.Name = "ValueBox";
+            this.ValueBox.ReadOnly = true;
             this.ValueBox.Size = new System.Drawing.Size(403, 50);
             this.ValueBox.TabIndex = 77;
             this.ValueBox.Text = "0";
